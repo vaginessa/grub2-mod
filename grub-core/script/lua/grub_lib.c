c0e0170166b2224b9b5d14f16b4a83c3f1177925
a1ive <ligenlive@gmail.com>
2018-02-06 13:54:32 +0000

    video.info()

--- grub-core/script/lua/grub_lib.c
+++ grub-core/script/lua/grub_lib.c
@@ -22,6 +22,7 @@
 #include "grub_lib.h"
 #include "gbk.h"
 
+#include <grub/dl.h>
 #include <grub/env.h>
 #include <grub/parser.h>
 #include <grub/command.h>
@@ -1159,88 +1160,150 @@ luaL_Reg grub_lua_lib[] =
 
 /* Lua function: get_time_ms() : returns the time in milliseconds.  */
 static int
-l_sys_get_time_ms (lua_State *L)
+lua_sys_get_time_ms (lua_State *state)
 {
-  lua_pushinteger (L, grub_get_time_ms ());
+  lua_pushinteger (state, grub_get_time_ms ());
   return 1;
 }
 
 /* Lua function: input.getkey() : returns { ASCII char, scan code }.  */
 static int
-l_input_getkey (lua_State *L)
+lua_input_getkey (lua_State *state)
 {
   int c = grub_getkey();
-  lua_pushinteger (L, c & 0xFF);          /* Push ASCII character code.  */
-  lua_pushinteger (L, (c >> 8) & 0xFF);   /* Push the scan code.  */
+  lua_pushinteger (state, c & 0xFF);          /* Push ASCII character code.  */
+  lua_pushinteger (state, (c >> 8) & 0xFF);   /* Push the scan code.  */
   return 2;
 }
 
 /* Lua function: video.swap_buffers().  */
 static int
-l_video_swap_buffers (lua_State *L)
+lua_video_swap_buffers (lua_State *state)
 {
   if (grub_video_swap_buffers () != GRUB_ERR_NONE)
-    return luaL_error (L, "Error swapping video buffers: %s", grub_errmsg);
+    return luaL_error (state, "Error swapping video buffers: %s", grub_errmsg);
   return 0;
 }
 
 static grub_video_color_t
-check_grub_color (lua_State *L, int narg)
+check_grub_color (lua_State *state, int narg)
 {
   /* Get the color components.  */
-  luaL_argcheck (L, lua_istable (L, narg), narg, "should be a color");
-  lua_getfield (L, narg, "r");
-  lua_getfield (L, narg, "g");
-  lua_getfield (L, narg, "b");
-  lua_getfield (L, narg, "a");
+  luaL_argcheck (state, lua_istable (state, narg), narg, "should be a color");
+  lua_getfield (state, narg, "r");
+  lua_getfield (state, narg, "g");
+  lua_getfield (state, narg, "b");
+  lua_getfield (state, narg, "a");
   grub_video_color_t color;
-  color = grub_video_map_rgba (luaL_checkint (L, -4),
-                               luaL_checkint (L, -3),
-                               luaL_checkint (L, -2),
-                               luaL_optint (L, -1, 255));
-  lua_pop (L, 4);
+  color = grub_video_map_rgba (luaL_checkint (state, -4),
+                               luaL_checkint (state, -3),
+                               luaL_checkint (state, -2),
+                               luaL_optint (state, -1, 255));
+  lua_pop (state, 4);
   return color;
 }
 
 /* Lua function: video.fill_rect(color, x, y, width, height). */
 static int
-l_video_fill_rect (lua_State *L)
+lua_video_fill_rect (lua_State *state)
 {
-  grub_video_color_t color = check_grub_color (L, 1);
-  int x = luaL_checkint (L, 2);
-  int y = luaL_checkint (L, 3);
-  int w = luaL_checkint (L, 4);
-  int h = luaL_checkint (L, 5);
+  grub_video_color_t color = check_grub_color (state, 1);
+  int x = luaL_checkint (state, 2);
+  int y = luaL_checkint (state, 3);
+  int w = luaL_checkint (state, 4);
+  int h = luaL_checkint (state, 5);
   if (grub_video_fill_rect (color, x, y, w, h) != GRUB_ERR_NONE)
-    return luaL_error (L, "Error filling rectangle: %s", grub_errmsg);
+    return luaL_error (state, "Error filling rectangle: %s", grub_errmsg);
   return 0;
 }
 
 /* Lua function: video.draw_string(text, font, color, x, y). */
 static int
-l_video_draw_string (lua_State *L)
+lua_video_draw_string (lua_State *state)
 {
-  grub_font_draw_string (luaL_checkstring (L, 1),
-                          grub_font_get (luaL_checkstring (L, 2)),
-                          check_grub_color (L, 3),
-                          luaL_checkint (L, 4),
-                          luaL_checkint (L, 5));
+  grub_font_draw_string (luaL_checkstring (state, 1),
+                          grub_font_get (luaL_checkstring (state, 2)),
+                          check_grub_color (state, 3),
+                          luaL_checkint (state, 4),
+                          luaL_checkint (state, 5));
   return 0;
 }
 
+struct hook_ctx
+{
+  unsigned int len;
+  char* data;
+};
+
+static int
+hook (const struct grub_video_mode_info *info, void *hook_arg)
+{
+  unsigned int len;
+  struct hook_ctx *ctx = hook_arg;
+  char buf[24];
+  if (info->mode_type & GRUB_VIDEO_MODE_TYPE_PURE_TEXT)
+    return 0;
+  grub_snprintf (buf, sizeof(buf), "%dx%dx%d ", info->width, info->height, info->bpp);
+  len = grub_strlen(buf);
+  if (ctx->data)
+    {
+      grub_strcpy(ctx->data + ctx->len, buf);
+    }
+  ctx->len += len;
+  return 0;
+}
+
+/* Lua function: video.info(). */
+static int
+lua_video_info (lua_State *state __attribute__ ((unused)))
+{
+  grub_video_adapter_t adapter;
+  grub_video_driver_id_t id;
+  struct hook_ctx ctx;
+#ifdef GRUB_MACHINE_PCBIOS
+  grub_dl_load ("vbe");
+#endif
+  id = grub_video_get_driver_id ();
+  FOR_VIDEO_ADAPTERS (adapter)
+  {
+    if (adapter->id != id && (id != GRUB_VIDEO_DRIVER_NONE || adapter->init() != GRUB_ERR_NONE))
+      {
+        continue;
+      }
+    ctx.data = NULL;
+    ctx.len = 0;
+    adapter->iterate (hook, &ctx);
+    ctx.data = grub_malloc(ctx.len+1);
+    ctx.len = 0;
+    adapter->iterate (hook, &ctx);
+    if (adapter->id != id)
+      adapter->fini();
+    if (id != GRUB_VIDEO_DRIVER_NONE || ctx.len)
+      {
+        lua_pushstring (state, ctx.data);
+        grub_free(ctx.data);
+	break;
+      }
+    else
+      grub_free(ctx.data);
+  }
+  return 1;
+}
+
 luaL_reg syslib[] = {
-    {"get_time_ms", l_sys_get_time_ms},
+    {"get_time_ms", lua_sys_get_time_ms},
     {0, 0}
 };
 
 luaL_reg inputlib[] = {
-    {"getkey", l_input_getkey},
+    {"getkey", lua_input_getkey},
     {0, 0}
 };
 
 luaL_reg videolib[] = {
-    {"swap_buffers", l_video_swap_buffers},
-    {"fill_rect", l_video_fill_rect},
-    {"draw_string", l_video_draw_string},
+    {"swap_buffers", lua_video_swap_buffers},
+    {"fill_rect", lua_video_fill_rect},
+    {"draw_string", lua_video_draw_string},
+    {"info", lua_video_info},
     {0, 0}
 };
